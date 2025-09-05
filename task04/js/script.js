fef9f3f65f9c49630838c5fc115bbdf47c986854
m.ruban <m.ruban@testportal.gov.ua>
2025-09-05 13:49:10 +0200

    Add initial project files for Cash Register including HTML, CSS, and JavaScript

---
+++ task04/js/script.js
@@ -0,0 +1,22 @@
+let price = 3.26;
+let cid = [
+  ['PENNY', 0.01],
+  ['NICKEL', 0.05],
+  ['DIME', 0.1],
+  ['QUARTER', 0.25],
+  ['ONE', 1],
+  ['FIVE', 5],
+  ['TEN', 10],
+  ['TWENTY', 20],
+  ['ONE HUNDRED', 100],
+];
+
+const output = document.getElementById('change-due');
+const cash = document.getElementById('cash');
+const purchaseBtn = document.getElementById('purchase-btn');
+const priceScreen = document.getElementById('price-screen');
+const cashDrawerDisplay = document.getElementById('cash-drawer-display');
+
+purchaseBtn.addEventListener('click', () => {
+  output.innerText = '';
+});
